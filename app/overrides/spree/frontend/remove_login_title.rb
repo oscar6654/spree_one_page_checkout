7e31477e8c558ea7502f3aa34304fb85138f29a7
Parth Barot <parth.barot@botreeconsulting.com>
2017-02-16 12:40:49 +0530

    Removing registration templates and overrides.

--- app/overrides/spree/frontend/remove_login_title.rb
+++
@@ -1,4 +0,0 @@
-# Deface::Override.new :virtual_path => 'spree/user_sessions/new',
-#                      :name => 'remove_login_title',
-#                      :replace_contents => "#existing-customer h6",
-#                      :text => ""
